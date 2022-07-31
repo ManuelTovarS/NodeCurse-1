3b115d7e74fc66afddfe6e77a0fa1b62a24fb5d6
Manuel J. Tovar <manueljosetovar@gmail.com>
2022-07-31 17:18:59 -0500

    Changes into the playground folder, use of json methods and read of files

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,13 +1,23 @@
 const fileSystem = require('fs');
 
-const book = {
+/* const book = {
   title: "El Quijote de la Mancha",
   author: "Miguel de Cervantes",
 };
-
 const bookString = JSON.stringify(book);
+fileSystem.writeFileSync('../playground/note.json', bookString); */
 
+/* const dataBuffer = fileSystem.readFileSync('./note.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON); */
 
-fileSystem.writeFileSync('../playground/note.json', bookString);
+//Challenge
+const dataJSON = fileSystem.readFileSync('note.json').toString();
+const data = JSON.parse(dataJSON);
+data.name = "Manuel J. Tovar";
+data.age = 28;
+data.planet = "Jupiter";
 
+const newJSON = JSON.stringify(data);
 
+fileSystem.writeFileSync('note2.json', newJSON);
