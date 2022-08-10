5ae9ee5947a781c80664b18ed72a2028a5dbc21b
Manuel J. Tovar <manueljosetovar@gmail.com>
2022-08-09 21:03:35 -0500

    Arrow functions review

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -1,7 +1,26 @@
-const square  = function (x) {
+/*const square = function (x) {
+  return x * x;
+};
+*/
 
+/* const square = (x) => {
     return x * x;
+} */
+/* 
+const square = (x) => x * x;
 
+console.log(square(3)); */
+
+
+const events = {
+    name: 'Birthday party',
+    guestList: ['Manuel', 'Andres', 'Kevin', 'Camilo', 'Pedro'],
+    printGuestList () {
+        console.log('Guest list for ', this.name);
+        this.guestList.forEach( (guest) => {
+            console.log(guest + ' is attending ' + this.name); 
+        });
+    }
 }
 
-console.log(square(2));
+events.printGuestList();
