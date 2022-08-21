916eb18ba6f65b981fb2dd11ccd197aa5cd83c81
Manuel J. Tovar <manueljosetovar@gmail.com>
2022-08-21 10:16:51 -0500

    End of the first project 'notes' in NodeJS curse

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,29 +1,8 @@
 //NPM Modules
-const validator = require("validator");
 const yargs = require("yargs");
 //Own Modules
-const { addNote, removeNote, listNotes, getNotes } = require("./notes");
+const { addNote, removeNote, listNotes, readNote } = require("./notes");
 const { argv } = require("process");
-
-//fileSystem (core package)
-/* fileSystem.writeFileSync('note.txt', 'Hello this is a note created by manuel jose tovar ');
-fileSystem.appendFileSync('note.txt', ',and this is the continue XD'); */
-
-//Example validator (npm package)
-/* const email = "manueljosetovar@gmailcom";
-const url = "https://google.com";
-console.log(validator.isEmail(email));
-console.log(validator.isURL(url)); */
-
-//Example of the use us chalk (npm package)
-/* console.log(chalk.red.bold.inverse('Chalk use!')); */
-
-//Use of the yargs, start of the project
-/* console.log(process.argv);
-console.log(process.argv[2]); */
-
-//Change the version of the yargs
-yargs.version("1.1.0");
 
 //Create add command
 yargs.command({
@@ -59,20 +38,25 @@ yargs.command({
   handler: (argv) => removeNote(argv.title),
 });
 
-//Create read command
-yargs.command({
-  command: "read",
-  description: "Read a note",
-  handler: function () {
-    console.log("Reading the note");
-  },
-});
-
 //Create list command
 yargs.command({
   command: "list",
   description: "List your notes",
   handler: () => listNotes(),
+});
+
+//Create read command
+yargs.command({
+  command: "read",
+  description: "Read a note",
+  builder: {
+    title: {
+      describe: "This option allow you to read a note by enter the title",
+      type: "string",
+      demandOption: true,
+    },
+  },
+  handler: (argv) => readNote(argv.title),
 });
 
 yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,32 +4,6 @@ const fileSystem = require("fs");
 const chalk = require("chalk");
 
 /**
- * Method to add a note, it takes two parameters
- * @param {string} title - the title of the note
- * @param {string} body - the body or the content of the note
- */
-const addNote = (title, body) => {
-  const notes = loadNotes();
-
-  const duplicateNote = notes.find(
-    (note) => note.title.toLowerCase() === title.toLowerCase()
-  );
-
-  if (!duplicateNote) {
-    notes.push({
-      title,
-      body,
-    });
-    saveNotes(notes);
-    console.log(chalk.green.bold("Note add succesfully!"));
-  } else {
-    return console.log(
-      chalk.yellow.bold("The note already exist, please pick another title!")
-    );
-  }
-};
-
-/**
  * Method to load existing notes by reading the JSON file called notes.json
  * @return {Array}
  */
@@ -59,6 +33,36 @@ const saveNotes = (notes) => {
 };
 
 /**
+ * Method to add a note, it takes two parameters
+ * @param {string} title - the title of the note
+ * @param {string} body - the body or the content of the note
+ */
+const addNote = (title, body) => {
+  const notes = loadNotes();
+
+  const duplicateNote = notes.find(
+    (note) => note.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (!duplicateNote) {
+    notes.push({
+      title,
+      body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green.bold("Note add succesfully!"));
+  } else {
+    return console.log(
+      chalk.yellow.bold(
+        "The note with title: " +
+          chalk.red.inverse(title) +
+          ", already exist please pick another title!"
+      )
+    );
+  }
+};
+
+/**
  * Method to delete a note by title
  * @param {string} title the title of the note to delete
  */
@@ -73,8 +77,8 @@ const removeNote = (title) => {
     console.log(
       chalk.yellow.bold(
         "There is no note with the following title: ",
-        title,
-        ",nothing change"
+        chalk.red.inverse(title),
+        ", nothing change"
       )
     );
   } else {
@@ -82,7 +86,7 @@ const removeNote = (title) => {
       chalk.green.bold(
         "The note with the following title: ",
         title,
-        "has been deleted"
+        ", has been deleted"
       )
     );
     saveNotes(newNotes);
@@ -99,18 +103,39 @@ const listNotes = () => {
 
   console.log(chalk.green.bold("Your notes are:"));
   notes.forEach((note) => {
-    console.log(chalk.yellow.bold(count + '-' + note.title));
+    console.log(chalk.yellow.bold(count + "-" + note.title));
     count++;
   });
 };
 
-const getNotes = () => {
-  return "Your notes...";
+/**
+ * Method to show the title and body of the note in the terminal
+ * @param {string} title the title of the note to read
+ */
+const readNote = (title) => {
+  const notes = loadNotes();
+
+  const noteToRead = notes.find(
+    (note) => note.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (noteToRead) {
+    console.log(chalk.green.bold(noteToRead.title));
+    console.log(chalk.grey.underline("-", noteToRead.body));
+  } else {
+    console.log(
+      chalk.red.bold(
+        "The note with the title: " +
+          chalk.red.inverse("'" + title + "'") +
+          " doesn't exist"
+      )
+    );
+  }
 };
 
 module.exports = {
-  getNotes,
   addNote,
   removeNote,
   listNotes,
+  readNote,
 };
